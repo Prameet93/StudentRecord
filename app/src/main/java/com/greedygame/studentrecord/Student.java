105311033da5827d723ca49df25925744001cfec
Prameet31 <prameet31o@gmail.com>
2017-04-03 20:32:32 +0530

    first commit

---
+++ app/src/main/java/com/greedygame/studentrecord/Student.java
@@ -0,0 +1,21 @@
+package com.greedygame.studentrecord;
+
+/**
+ * Created by Mathan on 30-Mar-17.
+ */
+
+public class Student {
+    public static final String TABLE = "Student";
+
+    // Labels Table Columns names
+    public static final String KEY_ID = "id";
+    public static final String KEY_name = "name";
+    public static final String KEY_email = "email";
+    public static final String KEY_age = "age";
+
+    // property help us to keep data
+    public int student_ID;
+    public String name;
+    public String email;
+    public int age;
+}
